4a1e796db51e64549ed52e50dbe64531455e31be
Jun0613-spec <kumo5110@gmail.com>
2023-08-01 09:28:38 +0100

    fixed

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -39,7 +39,7 @@ export const LandingHero = () => {
         </Link>
       </div>
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
-        No credit card required.
+        Unlock the power of AI for free.
       </div>
     </div>
   );
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -5,6 +5,7 @@ import { Moon, Sun } from "lucide-react";
 
 export function ThemeToggle() {
   const { setTheme } = useTheme();
+
   return (
     <div className="flex justify-center items-center gap-2 p-4">
       <Button
